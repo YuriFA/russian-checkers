e7a0a87cb60e275ab6d77bdddddeb7a82f5a1b07
YuriFA <yurik1452@mail.ru>
2016-12-14 15:28:03 +0300

    init2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 var N = 8;
 var WH = 100/N;
+var COLORS = {
+    "check": {
+        "red": "red",
+        "blue": "blue"
+    },
+    "cell": {
+        "black": "black",
+        "white": "white"
+    }
+};
+
 
 function drawBoard(n) {
     var board = document.createElement('div');
@@ -10,7 +21,7 @@ function drawBoard(n) {
         row.style.height = WH + '%';
         for (var j = 1; j <= n; j++) {
             var isBlack = j % 2 == i % 2;
-            var cell = drawCell(isBlack ? "black" : "white", isBlack && (i < 4 || i > 5), i < 4? 'blue' : 'red');
+            var cell = drawCell(isBlack ? COLORS.cell.black : COLORS.cell.white, isBlack && (i < 4 || i > 5), i < 4? COLORS.check.blue : COLORS.check.red);
             cell.style.width = WH + '%';
             row.appendChild(cell);
         }
@@ -19,12 +30,28 @@ function drawBoard(n) {
     return board;
 }
 
+function checkClickHandle(event) {
+    var actives = document.querySelector('.check.active');
+    if(actives !== null){
+        actives.classList.remove('active');
+    }
+    this.classList.toggle('active');
+}
+
+function cellClickHandle(event) {
+    if(this.children.length == 0)
+}
+
 function drawCell(color, withCheck=false, checkColor) {
     var cell = document.createElement('div');
     cell.className = "cell cell__" + color;
+    if(color == COLORS.cell.black) {
+        cell.addEventListener('click', cellClickHandle);
+    }
     if ( withCheck ) {
         var check = document.createElement('div');
         check.className = "check check__" + checkColor;
+        check.addEventListener('click', checkClickHandle);
         cell.appendChild(check);
     }
     return cell;
